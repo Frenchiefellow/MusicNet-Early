9392878018d3f27d3969571ebbadcb3dfab6e33f
Frenchiefellow <crpeters@umass.edu>
2014-04-05 18:04:23 -0400

    Removed Database credentials

--- adduser.php
+++ adduser.php
@@ -1,12 +1,12 @@
 <?php 
-$connection = mysql_connect("cs445sql", "crpeters", "EL159crp");
+$connection = mysql_connect(/*Removed*/);
   if (!$connection){
     die ("Couldn't connect to mysql server!<br>The error was: " . mysql_error());
   }
   else{
     echo "Connection successful!<br>\n";
   }
-   if (!mysql_select_db("clp"))
+   if (!mysql_select_db(/*Removed*/))
     die ("Couldn't select a database!<br> Error: " . mysql_error());
   else
     echo "Database selected successfully.<br>\n";
