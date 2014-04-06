f03d22d4955736c18e54ef01030c3482996f2726
Frenchiefellow <crpeters@umass.edu>
2014-04-06 17:16:56 -0400

    Sign-up and Logged-in Logic
    Added logic to destroy session if user is trying to sign up a new
    account, but already logged into another account.

--- adduser.php
+++ adduser.php
@@ -1,4 +1,19 @@
 <?php 
+
+//Destroys session if user is already signed in
+if(isset($_SESSION)){
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+session_unset();
+session_destroy()
+}
+
+//Information from the form 
 $user = $_POST['username'];
 $pass = $_POST['password'];
 $cpas = $_POST['conpassword'];
@@ -6,6 +21,7 @@ $age = $_POST['age'];
 $loc = $_POST['location'];
 $gen = $_POST['Gender'];
 
+//Test connection to my Database
 $connection = mysql_connect(/*removed*/);
   if (!$connection){
     die ("Couldn't connect to mysql server!<br>The error was: " . mysql_error());
@@ -13,20 +29,26 @@ $connection = mysql_connect(/*removed*/);
   else{
     echo "Connection successful!<br>\n";
   }
+
+//Test connect to Project Database
    if (!mysql_select_db(/*removed*/))
     die ("Couldn't select a database!<br> Error: " . mysql_error());
   else
     echo "Database selected successfully.<br>\n";
+
+//Define the query; Searches for Users with desired input
 	$db = /*removed*/;
 	$sql = "Select * From User where username = '$user'";
 	$result = mysql_query($sql);
 
+//If we can't find a table
 if (!$result) {
     echo "DB Error, could not list tables\n";
     echo 'MySQL Error: ' . mysql_error();
     exit;
 }
 
+//If we find some tuple, alert user with JavaScript Alert and redirect them to sign in page
 if(mysql_num_rows($result) > 0){
 ?>
  <script type="text/javascript">
@@ -35,7 +57,9 @@ if(mysql_num_rows($result) > 0){
 </script>
 <?php
 }
+
 else{
+//Make sure password and confirm password are the same, else JavaScript alert the user they are different
 if(strcmp($pass, $cpas) == 1){
 ?>
  <script type="text/javascript">
@@ -45,7 +69,7 @@ if(strcmp($pass, $cpas) == 1){
 <?php
 }
 else{
-
+//Define the boolean for gender
 $gender;
 if($gen !== "male"){
 $gender = 0;
@@ -54,13 +78,20 @@ else{
 $gender = 1;
 }
 
-
+//Insert the tuple into the User table
 $quer ='INSERT INTO User (loginacct, username, password, age, ismale, issuper, userloc) VALUES ("$user","$user", "$password", "$a", "$gender", "$loc")';
 if (!mysql_query($quer))
   {
   die('Error: ' . mysql_error());
   }
 echo "1 record added"; 
+
+//start a new session, store username inside for varius purposes, and redirect to newuser.php
+session_start();
+$_SESSION['username'] = $user;
+header('Location: newuser.php');
+die();
+
 }
 }
 
