d31712cd0737666196e4f8eb5ccf7ad7cc29b893
Frenchiefellow <crpeters@umass.edu>
2014-04-06 13:08:58 -0400

    Updated Active Class for Profile Sidebar
    The lists are appropriately highlighted on their respective pages.
    (active classes added where appropriate)

--- partials/sideBarProfile.php
+++ partials/sideBarProfile.php
@@ -1,12 +1,13 @@
 
+
 <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
 	     <li><a class = "user"><?php if(isset($_SESSION['username'])){echo $_SESSION['username'];} else{ echo "undefined";}; ?></a></li>
-            <li class="active"><a href="#">Overview</a></li>
-            <li><a href="#">Friends</a></li>
-            <li><a href="#">Playlists</a></li>
-            <li><a href="#">Songs</a></li>
-            <li><a href="">Stats</a></li>
+            <li <?php $url = "$_SERVER[REQUEST_URI]"; if(strpos($url, 'profile.php')){echo 'class = "active"';} ?>><a href="http://cs445.cs.umass.edu/php-wrapper/clp/profile.php">Overview</a></li>
+            <li <?php $url = "$_SERVER[REQUEST_URI]"; if(strpos($url, 'friends.php')){echo 'class = "active"';} ?>><a href="http://cs445.cs.umass.edu/php-wrapper/clp/friends.php">Friends</a></li>
+            <li <?php $url = "$_SERVER[REQUEST_URI]"; if(strpos($url, 'Playlists.php')){echo 'class = "active"';} ?>><a href="http://cs445.cs.umass.edu/php-wrapper/clp/profilePlaylists.php">Playlists</a></li>
+            <li <?php $url = "$_SERVER[REQUEST_URI]"; if(strpos($url, 'Songs.php')){echo 'class = "active"';} ?>><a href="http://cs445.cs.umass.edu/php-wrapper/clp/profileSongs.php">Songs</a></li>
+            <li <?php $url = "$_SERVER[REQUEST_URI]"; if(strpos($url, 'stats.php')){echo 'class = "active"';} ?>><a href="http://cs445.cs.umass.edu/php-wrapper/clp/stats.php">Stats</a></li>
           </ul>
     
         </div>
