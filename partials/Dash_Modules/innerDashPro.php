3a75c8649a4595219609d3e6a04c398f597a6eda
Frenchiefellow <crpeters@umass.edu>
2014-04-06 17:34:52 -0400

    Minor Profile Updates
    Will finish soon

--- partials/Dash_Modules/innerDashPro.php
+++ partials/Dash_Modules/innerDashPro.php
@@ -4,16 +4,17 @@
 
 <div class="row">
     <div class="row">
-        <div class="col-lg-6" style="border: 2px solid; border-radius: 10px; background-color: #D3D3D3; left: 1%;">
-          <img class="img"   src="data:image/jpg;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/girl.jpg"));?>" style="width: 140px; height: 140px;">
-          <h2><?php if(isset($_SESSION['username'])){echo $_SESSION['username'];} else{ echo "undefined";}; ?></h2>
+        <div class="col-lg-3" style="border: 2px solid; border-radius: 10px; background-color: #D3D3D3; left: 1%;">
+          <img class="img"   src="data:image/jpg;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/girl.jpg"));?>" style="width: 200px; height: 200px; border: 2px solid; border-radius: 2px; margin-top: 2%">
+          <h2 style="position: relative;"><?php if(isset($_SESSION['username'])){echo $_SESSION['username'];} else{ echo "undefined";}; ?></h2>
           <p>Welcome to <?php if(isset($_SESSION['username'])){echo $_SESSION['username'];} else{ echo "undefined";}; ?>'s MusicNet Profile Page! </p>
           <p>Here you can learn a little bit more about <?php if(isset($_SESSION['username'])){echo $_SESSION['username'];} else{ echo "undefined";}; ?>'s taste in music, and perhaps you'll find a common link! Take a look around, but don't be afraid to friend them. Don't 
 		worry, its not creepy! We are all music lovers here! </p>
-	   <p><a class="btn btn-default" href="#" role="button">Friend Me!</a></p>
+	   <p><a class="btn btn-primary" href="#" role="button">Friend Me!</a></p>
         </div>
 
-        <div class="col-lg-6">
+        <div class="col-lg-6"  style="border: 2px solid; border-radius: 10px; background-color: #D3D3D3; margin-left: 10%">
+	 <h2 style = "text-align: center; font-weight= bold; text-decoration:underline; font-family: 'Segoe UI', Frutiger, 'Frutiger Linotype', 'Dejavu Sans', 'Helvetica Neue', Arial, sans-serif;">About <?php if(isset($_SESSION['username'])){echo $_SESSION['username'];} else{ echo "undefined";}; ?>:</h2>
   
         </div>
          </div>
