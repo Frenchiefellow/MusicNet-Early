c87887c442589694a2bef36e51da1c7fa9549c57
Frenchiefellow <crpeters@umass.edu>
2014-04-09 17:01:02 -0400

    Formatting of Results in Admin.php
    Results now return in a table for easier viewing. Other minor formatting
    changes and stuff too.

--- admin.php
+++ admin.php
@@ -1,3 +1,10 @@
+<?php 
+if(!isset($_SESSION)){
+session_start();
+}
+echo '<form action="http://cs445.cs.umass.edu/php-wrapper/clp/profile.php?user=' . $_SESSION['username'] . '" class="navbar-form navbar-right" role="form" method="post">' .
+      '<button type="submit" class="btn btn-success" >Back to Profile</button></form>'; ?>
+
 <form method="post" action="<?php if(isset($_SESSION)){$si = session_id(); echo 'admin.php?id=' . $si;} ?>">
 <textarea name="query" cols="100" rows="5" placeholder="Enter Query, followed by ;">
 </textarea><br>
@@ -48,13 +55,33 @@ if (!$result) {
     echo 'MySQL Error: ' . mysql_error();
     exit;
 }
-while($row = mysql_fetch_assoc($result)){
-    foreach($row as $cname => $cvalue){
-        print "$cname: $cvalue\t";
-    }
-    print "<br>\r\n";
-}
- echo round($totaltime, 3, PHP_ROUND_HALF_UP) . " seconds";
+echo '<table border = "1" > <tbody> <tr>';
+
+ $res = array();
+  while($row = mysql_fetch_assoc($result))
+  {
+     $res[] = $row;
+  }
+  $columns = array_keys(reset($res));
+
+  foreach($columns as $col)
+       {
+          echo "<th style='padding-left: 5px; padding-right: 5px;'>$col</th>";
+       }
+	    echo '</tr>';
+    foreach($res as $row)
+       {
+          echo "<tr>";
+          foreach($columns as $col)
+          {
+             echo "<td style='padding-left: 5px; padding-right: 5px;'>".$row[$col]."</td>";
+          }
+          echo "</tr>";
+       }
+
+ echo '</tr></tbody></table><br>';
+ echo "APPROXIMATE QUERY TIME: " . round( $totaltime, 3, PHP_ROUND_HALF_UP) . " seconds<br><br>";
+ echo 'QUERY STATUS: SUCCESS!';
 
 }
 }
@@ -64,6 +91,8 @@ echo ' Authenication: False';
 }
 }
 }
+echo '<form action="http://cs445.cs.umass.edu/php-wrapper/clp/profile.php?user' . $_SESSION['username'] . '" class="navbar-form navbar-right" role="form" method="post">' .
+      '<button type="submit" class="btn btn-success" >Back to Profile</button></form>'; 
 
 
 function contains($str, array $arr)
