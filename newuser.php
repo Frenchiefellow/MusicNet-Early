f18528e2f85cc999f67d54e9e9896b6396cf612c
Frenchiefellow <crpeters@umass.edu>
2014-04-09 21:50:11 -0400

    Splash and NewUser Done.
    Splash and NewUser pages complete. Splash.css reworked.

--- newuser.php
+++ newuser.php
@@ -5,53 +5,61 @@
 body{
 background-image: url('http://upload.wikimedia.org/wikipedia/commons/f/fd/Dr_Dog_Treasure_Island_Music_Festival.jpg');
 }
+.colHead {
+color: white;
+margin-left: 1%; 
+text-align: center; 
+text-shadow: -1px 0 black, 0 2px black, 2px 0 black, 0 -1px black;
+}
+
+.colPar{
+color: white; 
+text-align: center; 
+text-shadow: -1px 0 black, 0 2px black, 2px 0 black, 0 -1px black; 
+font-size: 125%;
+}
 </style>
 
-<div class ='entire'>
 
-
-<div class="jumbotron" id= "welcome" style="background-color: rgba(128, 128, 128, .5);">
+<div class="jumbotron"  style="background-color: rgba(128, 128, 128, .5); margin-top: -2%;">
       <div id = "splashContainer" class="container">
-        <h1>Welcome, <?php echo ($_GET['user']); ?> to MusicNet!</h1>
-	 
-	<!-- CONTENT INSIDE P TAG -->
-        <p>Information Here welcoming new user</p>
+        <h1>Welcome to MusicNet, <?php echo ($_GET['user']); ?>!</h1>
+        <p>Congratulations <?php echo ($_GET['user']); ?>!
+        You've just joined a community of over a million music lovers and counting. You can rate, play, and tag hundreds of thousands of your favorite songs.
+        Don't have a hundred thousand favorite songs? No problem! You'll discover all of the things MusicNet has to offer&mdash;from your favorite songs, 
+        a new artist, or even a genre you've never heard of. At MusicNet, we understand that there's more to music than just listening to music. 
+        Scroll down to explore all that MusicNet has to offer!</p>
         <p><a class="btn btn-warning btn-lg" role="button" href="http://cs445.cs.umass.edu/php-wrapper/clp/profile.php" style="">Profile!</a></p>
       </div>
 </div> 
 
-<div class="container marketing">
-<div class="row">
 
-	<div class="col-lg-4">
-          <img class="img-circle" src='data:image/png;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/home.png"));?>' style="width: 140px; height: 140px;">
-          <h2>Check Out Your Profile!</h2>
+<div class="row" style="margin-top: 5%; margin-left: .5%; width: 99%">
 
-	   <!-- CONTENT INSIDE P TAG -->
-          <p>Something about your profile! Specifically, we have overview (subsection of stats), friends, playlists, and stats page via the profile</p>
-          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+  <div class="col-lg-4" style="margin-bottom: 5%;">
+          <img class="img-circle" src='data:image/png;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/home.png"));?>' style="width: 200px; height: 200px; margin-left: 33%;">
+          <h2 class="colHead">Check Out Your Profile!</h2>
+          <p class="colPar">What does your music say about you? View the songs you listened to, friends you added, and playlists you created, in your profile.</p>
+          <p><a style="margin-left: 42%;" class="btn btn-primary" href="<?php echo 'http://cs445.cs.umass.edu/php-wrapper/clp/profile.php?user=' . $_SESSION['username'];?>" role="button">Profile!</a></p>
         </div>
 
-       <div class="col-lg-4">
-          <img class="img-circle" src='data:image/jpg;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/mic.jpg"));?>' style="width: 140px; height: 140px;">
-
-	   <!-- CONTENT INSIDE P TAG -->
-          <h2>Create Playlists!</h2>
-          <p>Something about creating and sharing playlists!</p>
-          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+       <div class="col-lg-4" style="margin-bottom: 5%;">
+          <img class="img-circle" src='data:image/jpg;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/mic.jpg"));?>' style="width: 200px; height: 200px; margin-left: 35%;">
+          <h2 class="colHead">Create Playlists!</h2>
+          <p class="colPar">Looking for that perfect combination of tunes to get you through your homework? 
+          Click here to create, share, and discover great playlists!</p>
+          <p><a style="margin-left: 42%;" class="btn btn-primary" href="<?php echo 'http://cs445.cs.umass.edu/php-wrapper/clp/profilePlaylists.php?user=' . $_SESSION['username'];?>" role="button">Playlists!</a></p>
         </div>
 
-    	<div class="col-lg-4">
-          <img class="img-circle" src='data:image/jpg;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/discover.jpg"));?>' style="width: 140px; height: 140px;">
-          <h2>Discover New Music!</h2>
-
-	   <!-- CONTENT INSIDE P TAG -->
-          <p>Something about discovering new music!</p>
-          <p><a class="btn btn-default" href="http://cs445.cs.umass.edu/php-wrapper/clp/discover.php" role="button">View details »</a></p>
+      <div class="col-lg-4" style="margin-bottom: 5%;">
+          <img class="img-circle" src='data:image/jpg;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/discover.jpg"));?>' style="width: 200px; height: 200px; margin-left: 33%;">
+          <h2 class="colHead">Discover New Music!</h2>
+          <p class="colPar">You'll never have to worry about listening to the same old songs again, <?php echo ($_GET['user']); ?>!. Discover to find all sorts of new songs to listen to!</p>
+          <p><a style="margin-left: 42%;" class="btn btn-primary" href="http://cs445.cs.umass.edu/php-wrapper/clp/discover.php" role="button">Discover!</a></p>
         </div>
-
+  </div>
       </div>
-</div>
-</div>
+
+
 
 <?php include '/courses/cs400/cs445/php-dirs/clp/www/partials/bottombar.php'; ?>
--- splash.php
+++ splash.php
@@ -2,7 +2,7 @@
 <style>
 <?php include '/courses/cs400/cs445/php-dirs/clp/www/bs/css/splash.css'; ?>
 </style> 
-<div>
+
 
 <?php include '/courses/cs400/cs445/php-dirs/clp/www/partials/navbarSplash.php'; ?> 
 
@@ -10,37 +10,35 @@
 <div class="jumbotron" id= "welcome" style="background-color: rgba(128, 128, 128, .6);">
       <div id = "splashContainer" class="container">
         <h1>Welcome to MusicNet!</h1>
-
-	 <!-- CONTENT INSIDE P TAG -->
-        <p>MusicNet is the premier Social Network for all your music needs! Write a generic description here!</p>
-        <p><a class="btn btn-warning btn-lg" role="button" href="http://cs445.cs.umass.edu/php-wrapper/clp/login.php" style="">Sign Up!</a></p>
+        <p style = 'font-size: 150%;'>MusicNet is the premier Social Network for all your music needs! 
+        We are a community of over a million music lovers and counting. Rate, play, and tag hundreds of thousands of your favorite songs.
+        Don't have a hundred thousand favorite songs? No problem! MusicNet makes it easy to discover songs&mdash;from your favorite artist, a new artist, or even a genre you've never heard of.
+        At MusicNet, we understand that there's more to music than just listening to music. Join today to find out why over a million other people choose MusicNet!</p>
+        <p><a class="btn btn-warning btn-lg" role="button" href="http://cs445.cs.umass.edu/php-wrapper/clp/login.php">Sign Up!</a></p>
       </div>
 </div> 
-<div class="container marketing">
-<div class="row">
-        <div class="col-lg-4">
-          <img class="img-circle" src='data:image/jpg;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/discover.jpg"));?>' style="width: 140px; height: 140px;">
 
-          <h2>Discover New Music!</h2>
-	   <!-- CONTENT INSIDE P TAG -->
-          <p>Something about discovering new music</p>
-          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+
+<div class="row" style="margin-top: 5%; margin-left: .5%; width: 99%">
+        <div class="col-lg-4" style="margin-bottom: 5%;">
+          <img class="img-circle" src='data:image/jpg;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/discover.jpg"));?>' style="width: 200px; height: 200px; margin-left: 33%;">
+          <h2 class="colHead">Discover New Music!</h2>
+          <p class="colPar">With MusicNet, you never have to worry about listening to the same old songs. Discover all sorts of new songs to listen to!</p>
+          <p><a style="margin-left: 42%;" class="btn btn-primary" href="http://cs445.cs.umass.edu/php-wrapper/clp/discover.php" role="button">Discover!</a></p>
         </div>
-        <div class="col-lg-4">
-          <img class="img-circle" src='data:image/jpg;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/concert.jpg"));?>' style="width: 140px; height: 140px;">
 
-	   <!-- CONTENT INSIDE P TAG -->
-          <h2>Recommended for You!</h2>
-          <p>Something about how we recommend music for the user based on tags etc!</p>
-          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+        <div class="col-lg-4" style="margin-bottom: 5%;">
+          <img class="img-circle" src='data:image/jpg;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/concert.jpg"));?>' style="width: 200px; height: 200px; margin-left: 35%;">
+          <h2 class="colHead">Recommended for You!</h2>
+          <p class="colPar">From the tags of songs that you liked, we crafted a list of other songs we think you might like too!</p>
+          <p><a style="margin-left: 42%;" class="btn btn-primary" href="http://cs445.cs.umass.edu/php-wrapper/clp/login.php" role="button">Sign Up First!</a></p>
         </div>
-        <div class="col-lg-4">
-          <img class="img-circle" src='data:image/jpg;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/girl.jpg"));?>' style="width: 140px; height: 140px;">
-          <h2>Connect With Friends!</h2>
 
-	   <!-- CONTENT INSIDE P TAG -->
-          <p>Something about connecting/making</p>
-          <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+        <div class="col-lg-4" style="margin-bottom: 5%;">
+          <img class="img-circle" src='data:image/jpg;base64,<?php echo base64_encode(file_get_contents("/courses/cs400/cs445/php-dirs/clp/www/resources/images/girl.jpg"));?>' style="width: 200px; height: 200px; margin-left: 33%;">
+          <h2 class="colHead">Connect With Friends!</h2>
+          <p class="colPar">What's better than listening to music? Sharing it with your friends! Click here to connect with other MusicNet patrons.</p>
+          <p><a style="margin-left: 42%;" class="btn btn-primary" href="http://cs445.cs.umass.edu/php-wrapper/clp/login.php" role="button">Sign Up First!</a></p>
         </div>
       </div>
 </div>
@@ -49,6 +47,6 @@
 
 <?php include "/courses/cs400/cs445/php-dirs/clp/www/partials/bottombar.php"; ?> 
 
-</div>
+
 
 
