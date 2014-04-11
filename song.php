e60e3d03ebe747aeacdb97cecbaf3fe57fa0880e
Frenchiefellow <crpeters@umass.edu>
2014-04-11 00:08:05 -0400

    Readded Check for Song.php

--- song.php
+++ song.php
@@ -1,3 +1,4 @@
+<?php include '/courses/cs400/cs445/php-dirs/clp/www/Scripts/checkMusic.php'; ?>
 <?php include '/courses/cs400/cs445/php-dirs/clp/www/partials/header.php'; ?>
 <?php include '/courses/cs400/cs445/php-dirs/clp/www/partials/navbarProfile.php'; ?>
 <style>
