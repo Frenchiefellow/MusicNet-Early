05f4387aba7b0b7eea349d0fb440384842413adc
Frenchiefellow <crpeters@umass.edu>
2014-04-21 21:25:51 -0400

    GLORIOUS ABOUT PAGE IS FINISHED

--- about.php
+++ about.php
@@ -1,4 +1,107 @@
 <?php include '/courses/cs400/cs445/php-dirs/clp/www/partials/header.php'; ?>
 <?php include '/courses/cs400/cs445/php-dirs/clp/www/partials/navbarProfile.php'; ?>
+<?php include '/courses/cs400/cs445/php-dirs/clp/www/partials/aboutside.php'; ?>
+<style>
+html, body{
+	height: 100%;
+	width: 100%;
+	margin: auto;
+	overflow: auto;
+	min-height: 100%;
+	padding-bottom: 60px;
+}
+</style>
+
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main" style="height: 100%;">
+	<div style="background-color: #ee; padding: 20px; border: 0px solid; border-radius: 10px;">
+
+	<div style="background-color: white; border: 0px solid; border-radius: 10px;">
+	<h1 id="about" class="page-header">About MusicNet</h1>
+	<p>
+		MusicNet&mdash;what started as just a project for a database course at UMass, has become so much more. Now it's a website too!
+	</p> 
+	<p>
+		Besides the lack of ability to stream music, an active user base, or any sort of customer service, MusicNet has as much to offer as Spotify did 8 years ago.
+		Look through our database of songs, artists, albums, and user interactions that would rival that of any other database that stores the exact same things
+		as ours but with less entries and in a far more efficient manor. As far as frontends on databases go, MusicNet definitely does. If you care to learn more about this bold venture,
+		read on <a href="http://www.youtube.com/watch?v=u9oj-iQNXKQ">(or don't, I'm not your dad)</a>.
+	</p><br>
+	</div>
+
+	<div style="background-color: white; border: 0px solid; border-radius: 10px;">
+	<h1 id="music" class="page-header">Music at MusicNet</h1>
+	<p>
+		Nowadays it's hard to trip without falling on a music streaming site. Why MusicNet? 
+		MusicNet has more songs than God himself. How many songs does God have? Let us define MusicNet as having n songs. God has n-1 songs. QED.
+	</p>
+	<p>
+		But seriously, we've got a lot of songs. 385 thousand or so may not seem like much, but it's a lot. We even include songs such as,
+		"Dustin Hoffman's Wife Makes A Sarcastic Remark Cuts The Head Off A Duck Places It Where The Tub Was and Begins To Growl" or, 
+		"Dustin Hoffman's Tongue Taken To Police Lab Where It Is Used As Toilet Paper and Reading Material While On The Toilet." Seriously,
+		these are songs that don't just exist in our database, they are real songs. It is a part of an album by the band <i>Of Montreal</i> in an
+		album where every song references Dustin Hoffman and his family. Google it or search on a useful site like Spotify. 
+	</p>
+
+	<p>	
+		It's a terrible song though, I wouldn't reccommend listening to it for too long. You'll be waiting and waiting for it to get to the good
+		part but it never comes. You'll see related videos on Youtube and you'll click to the next one thinking maybe it will be better, but it won't be.
+		It's like if everything that made the Beetles good was taken away&mdash;it's like a band of four Ringo Starrs got together just to ruin the 
+		10 minutes of your life that you devoted to listening to their music. Don't take my word for it, <a href="http://open.spotify.com/album/0hkdPlZ1durffV7QVIE8rR">go and see for yourself!</a>
+	</p><br>
+	</div>
+
+	<div style="background-color: white; border: 0px solid; border-radius: 10px;">
+	<h1 id="morethanmusic" class="page-header">More Than Just Music</h1>
+	<p>
+		If you are still not convinced that we are a <i>top tier</i> social networking site, that's probably because we haven't mentioned that we are a social networking site yet: We <i>are</i> a social networking site. We give you the freedom to rate songs that you've listened to with none of the incentive to. We also give you the abilty to rate songs you haven't listened to, in case you're a communist. Then again, the play button is just a listener that doesn't do anything, so I guess you can do what you want.
+	</p>
+	<p> <i>Commie</i> </p><br>
+	</div>
+
+	<div style="background-color: white; border: 0px solid; border-radius: 10px;">
+	<h1 id="dev" class="page-header">Meet the Development Team</h1>
+
+	<div id="chris" class="row" style="border: 2px solid; border-radius: 10px; width: 100%; margin-left: auto; margin-right: auto;">
+	<div class="col-sm-4" style="background-color: #808080">
+	<img src="http://cs445.cs.umass.edu/groups/clp/www/resources/images/ChrisAbout.png"  style="border: 0px solid; border-radius: 10px; width: 90%; height: 225px; "> 
+	</div>
+	<div class="col-sm-8">
+	<h4 style="text-align: center" class="page-header">CHRIS: Frontend Master, Backend Apprentice</h4>
+	<p style="float: right">
+	Chris is the slave driver. He <i>whipped</i> Piers and Ted into submission relentlessly until their fingers were only dust and the website was complete. Spent all his free time complaining that he was
+	the only one on the frontend and went <strong>WELL</strong> beyond the requirements, while still complaining that he had too much work to do. Chris now dreams in HTML, CSS, PHP, MySQL, and JavaScript and has 
+	increased his level of Monster Energy Drinks in his blood-stream from 15% to a solid 50%. In otherwords, I'm half caffeine and half terrible things for your body. Don't worry, I'm still here...for now.
+	</p>
+	</div>
+	</div>
+
+	<div id="ted" class="row" style="border: 2px solid; margin-top: 1%; border-radius: 10px; width: 100%; margin-left: auto; margin-right: auto;">
+	<div class="col-sm-4" style="background-color: #808080;" >
+	<img src="http://cs445.cs.umass.edu/groups/clp/www/resources/images/TedAbout.png"  style="border: 0px solid; border-radius: 10px;  width: 90%; height: 225px; "> 
+	</div>
+	<div class="col-sm-8">
+	<h4 style="text-align: center" class="page-header">TED: Document Master, Frontend Apprentice, Backend Apprentice</h4>
+	<p>
+	Ted is a straight male, age 20 with <i>meaty</i> hands that have a passion for nurturing and massaging a keyboard with sexy LaTeX documents and sensual MySQL queries. I wrote this page, but I didn't get to
+	write my own description. Just kidding, Chris did this, just like the entire Frontend. 
+	</p>
+	</div>
+	</div> 
+
+	<div id="piers" class="row" style="border: 2px solid; margin-top: 1%; border-radius: 10px; width: 100%; margin-left: auto; margin-right: auto;">
+	<div class="col-sm-4" style="background-color: #808080">
+	<img src="http://cs445.cs.umass.edu/groups/clp/www/resources/images/PiersAbout.png" style="border: 0px solid; border-radius: 10px;  width: 90%; height: 225px;"> 
+	</div>
+	<div class="col-sm-8">
+	<h4 style="text-align: center" class="page-header">PIERS: Backend Master</h4>
+	<p>
+	Born in the town of Smallville, Kansas Piers watched the brutal murder of his parents before his eyes while exiting a theatre. After meeting the sorcerer solomon, he was granted superhuman abilities using his ring of power. After defeating a number of supervillians, he met his greatest foe who was immune to his webslinging powers, Magneto, who could control his Unobtanium bones. Only with the help of his sidekick Speedy, was Ultron defeated and balance restored to the League of Shadows. Also, I did the database.
+	</p>
+	</div>
+	</div>
+	</div>
+	</div>
+</div>
+
 
 <?php include '/courses/cs400/cs445/php-dirs/clp/www/partials/bottombar.php'; ?>
---
+++ partials/aboutside.php
@@ -0,0 +1,13 @@
+<div class="col-sm-3 col-md-2 sidebar" >
+  <ul class="nav nav-sidebar">
+            <li><a href="#about">About Musicnet</a></li>
+            <li ><a href="#music">Music at MusicNet</a></li>
+            <li ><a href="#morethanmusic">More Than Just Music</a></li>
+            <li ><a href="#dev">Meet the Development Team</a></li>
+            	<ul >
+            		<li ><a href="#chris">Chris</a></li>
+            		<li ><a href="#ted">Ted</a></li>
+            		<li ><a href="#piers">Piers</a></li>
+            	</ul>
+          </ul>
+</div>
